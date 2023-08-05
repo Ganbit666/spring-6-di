7e864d39a9cb03a11eb5a1864b2398aafe003cc7
Ganbit <ganbit666@o2.pl>
2023-08-05 22:42:45 +0200

    End 33

--- src/main/java/guru/springframework/spring6di/controllers/MyController.java
+++ src/main/java/guru/springframework/spring6di/controllers/MyController.java
@@ -1,13 +1,22 @@
 package guru.springframework.spring6di.controllers;
 
+import guru.springframework.spring6di.services.GreetingService;
+import guru.springframework.spring6di.services.GreetingServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class MyController {
 
+    private final GreetingService greetingService;
+
+    public MyController() {
+        this.greetingService = new GreetingServiceImpl();
+    }
+
     public String sayHallow(){
         System.out.println("I'm in the controller");
 
-        return "Hello Everyone!!!";
+        return greetingService.sayGreeting();
     }
 }
